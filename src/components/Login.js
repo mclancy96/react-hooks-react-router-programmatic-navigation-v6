372138e874f402ab62fdefb043d09d56c1e3e52f
Mike <mclancy96@gmail.com>
2025-06-20 16:16:35 -0400

    Fix spacing

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,43 +2,43 @@ import { useState } from "react";
 import { useOutletContext } from "react-router-dom";
 
 function Login() {
-	const login = useOutletContext();
-	const [formData, setFormData] = useState({
-		username: "",
-		password: "",
-	});
-	console.log("navigated to login");
+  const login = useOutletContext();
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+  console.log("navigated to login");
 
-	function handleChange(e) {
-		setFormData({
-			...formData,
-			[e.target.name]: e.target.value,
-		});
-	}
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  }
 
-	function handleLogin(e) {
-		e.preventDefault();
-		login();
-	}
+  function handleLogin(e) {
+    e.preventDefault();
+    login();
+  }
 
-	return (
-		<form onSubmit={handleLogin}>
-			<h1>Login</h1>
-			<input
-				type="text"
-				name="username"
-				value={formData.username}
-				onChange={handleChange}
-			/>
-			<input
-				type="password"
-				name="password"
-				value={formData.password}
-				onChange={handleChange}
-			/>
-			<input type="submit" value="Login" />
-		</form>
-	);
+  return (
+    <form onSubmit={handleLogin}>
+      <h1>Login</h1>
+      <input
+        type="text"
+        name="username"
+        value={formData.username}
+        onChange={handleChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={formData.password}
+        onChange={handleChange}
+      />
+      <input type="submit" value="Login" />
+    </form>
+  );
 }
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import routes from "./routes"
 
 const router = createBrowserRouter(routes)
+console.log('router keys:', Object.keys(router))
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<RouterProvider router={router}/>)
